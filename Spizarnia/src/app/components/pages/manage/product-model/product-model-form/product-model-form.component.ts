cbaa25fa87b552a63841701a4f78c6e291dfbeeb
Jakub <jakub.jakacki18@gmail.com>
2024-11-28 12:43:13 +0100

    - komunikacja pomiędzy kontrollerami (odświeżanie listy)

--- Spizarnia/src/app/components/pages/manage/product-model/product-model-form/product-model-form.component.ts
+++ Spizarnia/src/app/components/pages/manage/product-model/product-model-form/product-model-form.component.ts
@@ -1,41 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductModelService } from './../../../../../services/product-model.service'; // Serwis do wysyłania danych
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-product-model-form',
-  imports: [ReactiveFormsModule, CommonModule],
-  templateUrl: './product-model-form.component.html',
-  styleUrl: './product-model-form.component.css'
+ selector: 'app-product-model-form',
+ imports: [ReactiveFormsModule, CommonModule],
+ templateUrl: './product-model-form.component.html',
+ styleUrl: './product-model-form.component.css'
 })
 export class ProductModelFormComponent {
-  productForm: any= undefined;  // Deklaracja formularza
+ productForm: any;  // Deklaracja formularza
+ @Output() productCreated = new EventEmitter<void>();
 
-  constructor(private fb: FormBuilder, private productModelService: ProductModelService) { }
+ constructor(private fb: FormBuilder, private productModelService: ProductModelService) { }
 
-  ngOnInit(): void {
-    // Inicjalizacja formularza
-    this.productForm = this.fb.group({
-      name: ['', Validators.required],
-      unit: ['', Validators.required],
-      price: ['', [Validators.required, Validators.min(0)]],
-      quantity: ['', [Validators.required, Validators.min(1)]]
-    });
+ngOnInit(): void {
+// Inicjalizacja formularza
+  this.productForm = this.fb.group({
+  name: ['', Validators.required],
+  unit: ['', Validators.required],
+  price: ['', [Validators.required, Validators.min(0)]],
+  quantity: ['', [Validators.required, Validators.min(1)]]
+  });
+}
+onSubmit() {
+  if (this.productForm.valid) {
+  // Wywołanie metody serwisu do wysyłania danych
+  this.productModelService.createProduct(this.productForm.value).subscribe(
+  (response) => {
+    console.log('Produkt został utworzony:', response);
+    this.productCreated.emit();
+  },
+  (error) => {
+    console.error('Błąd podczas tworzenia produktu:', error);
   }
-    onSubmit() {
-      if (this.productForm.valid) {
-        // Wywołanie metody serwisu do wysyłania danych
-        this.productModelService.createProduct(this.productForm.value).subscribe(
-          (response) => {
-            console.log('Produkt został utworzony:', response);
-          },
-          (error) => {
-            console.error('Błąd podczas tworzenia produktu:', error);
-          }
-        );
-      } else {
-        console.log('Formularz jest nieprawidłowy');
-      }
-  }
+ );
+ } else {
+    console.log('Formularz jest nieprawidłowy');
+ }
+ }
 }
