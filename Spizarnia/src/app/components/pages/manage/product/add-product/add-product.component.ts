294e85549668319e027fec769d6013a9ed061f39
Jakub <jakub.jakacki18@gmail.com>
2024-12-03 16:03:41 +0100

    - dodawanie daty

--- Spizarnia/src/app/components/pages/manage/product/add-product/add-product.component.ts
+++ Spizarnia/src/app/components/pages/manage/product/add-product/add-product.component.ts
@@ -3,12 +3,18 @@ import { Component, QueryList, ViewChildren } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatListModule, MatSelectionList, MatSelectionListChange} from '@angular/material/list';
 import {MatExpansionModule} from '@angular/material/expansion'
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {provideNativeDateAdapter} from '@angular/material/core';
 import { Category } from '../../../../../shared/models/Category';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-product',
-  imports: [MatListModule,FormsModule,ReactiveFormsModule,MatExpansionModule,CommonModule],
+  providers:[provideNativeDateAdapter()],
+  imports: [MatListModule,FormsModule,ReactiveFormsModule,MatExpansionModule,MatDatepickerModule,MatButtonModule,MatFormFieldModule,MatInputModule,CommonModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
@@ -28,23 +34,31 @@ export class AddProductComponent {
   @ViewChildren(MatSelectionList) selectionLists!: QueryList<MatSelectionList>;
 selectedProduct: any;
 onSubmit() {
-throw new Error('Method not implemented.');
-}
-  productForm: FormGroup;
   
+  
+  
+  throw new Error('Method not implemented.');
+}
+
+  productForm: FormGroup;
+  minDate : Date;
   categories: Category[] =[];
   ngOnInit()
   {  
     this.fetchCategories();
     this.productForm = this.fb.group({
       selectedProduct: ['', Validators.required],
+      dateOfExpiration: [this.minDate,Validators.required]
       });
   }
   categoriesControl = new FormControl();
+
   constructor(private fb: FormBuilder, private categoryService: CategoryService) {
     this.productForm = new FormGroup({
       categories: this.categoriesControl,
-    });}
+    });
+    this.minDate = new Date();
+  }
 
     fetchCategories(): void {
       this.categoryService.getAllCategories().subscribe({
