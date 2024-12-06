2463ca7fb96e83165a0e7e1977f906fa85b15bb5
JakubKazimiruk46 <154755194+JakubKazimiruk46@users.noreply.github.com>
2024-12-06 13:26:46 +0100

    Revert "Wyszukiwanie na productModel - na razie tylko w konsoli"
    This reverts commit eaa17d198a43db7dfb2c1f4ebf2faacfea84155f.

--- Spizarnia-backend/src/controllers/product.controller.ts
+++ Spizarnia-backend/src/controllers/product.controller.ts
@@ -6,12 +6,11 @@ import { ProductModel } from "../models/ProductModel";
 
 const productRepository: Repository<Product> = AppDataSource.getRepository(Product);
 const productModelRepository: Repository<ProductModel> = AppDataSource.getRepository(ProductModel);
-
 export const ProductController = {
   async getAll(req: Request, res: Response) {
     try {
       const products = await productRepository.find({
-        relations: ["productModel", "container"],
+        relations: ["productModel", "container", "shelf"],
       });
       res.json(products);
     } catch (error) {
@@ -24,7 +23,7 @@ export const ProductController = {
     try {
       const product = await productRepository.findOne({
         where: { id: parseInt(id) },
-        relations: ["productModel", "container"],
+        relations: ["productModel", "container", "shelf"],
       });
 
       if (!product) {
@@ -39,21 +38,29 @@ export const ProductController = {
   },
 
   async create(req: Request, res: Response) {
+    const { expirationDate, purchaseDate, selectedProduct } = req.body;
+    console.log([[selectedProduct[0].id]]);
     try {
-      const { expirationDate, purchaseDate, selectedProduct } = req.body;
+      //const selectedProductModel = productModelRepository.findOne({where: { id: parseInt(selectedProduct.id) }})
       const newProduct = productRepository.create({
         expirationDate: new Date(expirationDate),
         purchaseDate: new Date(purchaseDate),
+        //productModelId : selectedProduct.id
+        //containerId: selectedProduct.cate
         productModel: selectedProduct[0],
-      });
-  
+        /* TODO: podłaczyc z pozostałymi tabelami.
+        container: containerId ? { id: containerId } : null,
+        shelf: shelfId ? { id: shelfId } : null,
+        */
+    });
+
       await productRepository.save(newProduct);
       res.status(201).json(newProduct);
     } catch (error) {
       res.status(500).json({ error: "Internal error: Product was not created" });
     }
   },
-  
+
   async update(req: Request, res: Response) {
     const { id } = req.params;
     const { expirationDate, purchaseDate, productModelId, containerId, shelfId } = req.body;
