9993356241e3473d3cae863505a23919a00b9983
Bartłomiej Karanowski <aztek092016@gmail.com>
2024-12-06 09:59:01 +0100

    widok przepisów

--- Spizarnia/src/app/components/pages/recipes/recipes.component.ts
+++ Spizarnia/src/app/components/pages/recipes/recipes.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipes',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css'
+  styleUrls: ['./recipes.component.css'],
 })
 export class RecipesComponent {
+  recipes = [
+    { name: 'Spaghetti', ingredients: ['Makaron', 'Sos pomidorowy', 'Mięso'] },
+    { name: 'Omlet', ingredients: ['Jajka', 'Ser', 'Sól'] },
+  ];
 
+  filteredRecipes = this.recipes;
+
+  filterResults(query: string) {
+    query = query.toLowerCase();
+    this.filteredRecipes = this.recipes.filter(
+      (recipe) =>
+        recipe.name.toLowerCase().includes(query) ||
+        recipe.ingredients.some((ingredient) =>
+          ingredient.toLowerCase().includes(query)
+        )
+    );
+  }
+
+  resetSearch() {
+    this.filteredRecipes = this.recipes;
+  }
+
+  deleteRecipe(recipe: any) {
+    this.recipes = this.recipes.filter((r) => r !== recipe);
+    this.filteredRecipes = this.recipes;
+  }
 }
