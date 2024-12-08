56ff9171c06be596307861a0e7b3e388a4616cd8
Klaudia Klim <klaudia.n.klim@gmail.com>
2024-12-08 23:32:43 +0100

    - Added shelves on the home page, - Changed the header so the page title ain't so close to the left - When there are no shelves theres text with information

--- Spizarnia/src/app/components/pages/home/containers/containers.component.ts
+++ Spizarnia/src/app/components/pages/home/containers/containers.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ContainerService } from '../../../../services/container.service';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-containers',
-  imports: [RouterModule],
+  imports: [RouterModule , CommonModule],
   templateUrl: './containers.component.html',
-  styleUrl: './containers.component.css'
+  styleUrls: ['./containers.component.css']
 })
-export class ContainersComponent implements OnInit{
-  containers : any;
-  constructor(private containerService: ContainerService){}
-  ngOnInit()
-  {  
+export class ContainersComponent implements OnInit {
+  containers: any[] = [];
+  rows: any[] = [];
+
+  constructor(private containerService: ContainerService) { }
+
+  ngOnInit() {
     this.fetchContainers();
   }
 
   fetchContainers(): void {
     this.containerService.getAllContainers().subscribe({
-      next: (data) => (this.containers = data),
+      next: (data) => {
+        this.containers = data;
+        this.splitIntoRows(); // Podział na rzędy
+      },
       error: (err) => console.error('Error fetching Containers:', err),
     });
+  }
+
+  // Funkcja dzieląca kontenery na rzędy
+  splitIntoRows(): void {
+    const rowSize = 5; // Maksymalna liczba kontenerów w rzędzie
+    this.rows = [];
+
+    for (let i = 0; i < this.containers.length; i += rowSize) {
+      this.rows.push(this.containers.slice(i, i + rowSize));
+    }
   }
 }
