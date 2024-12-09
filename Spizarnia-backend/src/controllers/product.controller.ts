ef72bb324285b46e34003fc57392b0abba543c01
JakubKazimiruk46 <154755194+JakubKazimiruk46@users.noreply.github.com>
2024-12-09 17:44:52 +0100

    Poprawki w product
    Edycja product tylko expiration i purchase date, Edycja quantity, ceny itp to PUT na productModel
    Używamy /api/product/{product-id}
    albo /productModel/{productModel-id}

--- Spizarnia-backend/src/controllers/product.controller.ts
+++ Spizarnia-backend/src/controllers/product.controller.ts
@@ -85,25 +85,49 @@ export const ProductController = {
   
   async update(req: Request, res: Response) {
     const { id } = req.params;
-    const { expirationDate, purchaseDate, productModelId, containerId } = req.body;
-
+    const { expirationDate, purchaseDate, productModelId } = req.body;
+    if (!expirationDate || !purchaseDate) {
+      res.status(400).json({ error: "Missing required fields" });
+      return
+    }
+  
     try {
-      const product = await productRepository.findOneBy({ id: parseInt(id) });
-
+      const product = await productRepository.findOne({
+        where: { id: parseInt(id) },
+        relations: ['productModel'],
+      });
+  
       if (!product) {
         res.status(404).json({ error: `Product with id: ${id} was not found` });
-        return
+        return;
       }
-
+  
       product.expirationDate = new Date(expirationDate);
       product.purchaseDate = new Date(purchaseDate);
 
+      if (productModelId) {
+        const productModel = await productModelRepository.findOne({ where: { id: productModelId } });
+        if (!productModel) {
+          res.status(404).json({ error: `Product model with id: ${productModelId} was not found` });
+          return;
+        }
+        product.productModel = productModel;
+      }
+
       await productRepository.save(product);
-      res.json(product);
+  
+      res.json({
+        id: product.id,
+        expirationDate: product.expirationDate,
+        purchaseDate: product.purchaseDate,
+        productModel: product.productModel,
+      });
     } catch (error) {
+      console.error('Error updating product:', error);
       res.status(500).json({ error: "Internal error: Product was not updated" });
     }
   },
+  
 
   async delete(req: Request, res: Response) {
     const { id } = req.params;
