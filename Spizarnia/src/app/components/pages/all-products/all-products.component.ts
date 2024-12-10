24c4cde35600c8407dd5b4390b7be5b7343520ad
Klaudia Klim <klaudia.n.klim@gmail.com>
2024-12-10 23:40:21 +0100

    tabela all-products

--- Spizarnia/src/app/components/pages/all-products/all-products.component.ts
+++ Spizarnia/src/app/components/pages/all-products/all-products.component.ts
@@ -1,40 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { MatTableDataSource } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSort, MatSortModule, SortDirection } from '@angular/material/sort';
+import { Product } from '../../../shared/models/Product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-all-products',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatTableModule, MatSortModule],
   templateUrl: './all-products.component.html',
-  styleUrls: ['./all-products.component.css']
+  styleUrls: ['./all-products.component.css'],
+  providers: [DatePipe]
 })
 export class AllProductsComponent implements OnInit {
   products: any[] = [];
   displayedColumns: string[] = ['id', 'name', 'quantity', 'category', 'purchaseDate', 'expirationDate'];
-  dataSource = new MatTableDataSource<any>([]);
+  searchTerm: string = '';
+  dataSource = new MatTableDataSource<any>([]); // Użycie MatTableDataSource dla sortowania
 
-  constructor(private http: HttpClient) { }
+  @ViewChild(MatSort) sort!: MatSort; // Dodanie ViewChild do obsługi sortowania
+
+  constructor(private http: HttpClient, private datePipe: DatePipe) { }
+  private checkExpirationInterval: any;
 
   ngOnInit() {
-    this.getAllProducts();
+    this.getAllProducts()
+
+    this.checkExpirationInterval = setInterval(() => {
+      this.checkExpirationDates();
+    }, 60000); // 60000 ms = 1 minuta
+  }
+  ngOnDestroy() {
+    // Czyść interwał przy niszczeniu komponentu
+    if (this.checkExpirationInterval) {
+      clearInterval(this.checkExpirationInterval);
+    }
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort; // Ustawienie sortowania po inicjalizacji widoku
+  }
+
+  onSearch() {
+    if (this.searchTerm.length >= 3) {
+      const searchTermLower = this.searchTerm.toLowerCase();
+
+      this.dataSource.filter = searchTermLower; // Użycie wbudowanego filtrowania w MatTableDataSource
+      this.dataSource.filterPredicate = (data: any, filter: string) => {
+        return data.name.toLowerCase().includes(filter); // Wyszukiwanie po nazwie
+      };
+    } else {
+      this.dataSource.filter = ''; // Reset filtra
+    }
   }
 
   getAllProducts() {
-    this.http.get<any[]>('http://localhost:5000/api/product').subscribe(
+    this.http.get<any[]>(`http://localhost:5000/api/product`).subscribe(
       (data) => {
         this.products = data;
-        this.dataSource.data = data;
+        this.dataSource.data = data; // Przekazanie danych do MatTableDataSource
       },
       (error) => {
-        console.error('Błąd!', error);
+        console.error("Error!", error);
       }
-    );
+    )
   }
 
-  onSearch(searchTerm: string) {
-    if (searchTerm.length >= 3) {
-      this.dataSource.filter = searchTerm.toLowerCase();
-    } else {
-      this.dataSource.filter = '';
+  checkExpirationDates() {
+    const currentDate = new Date();
+    const expiringProducts: Product[] = [];
+
+    this.products.forEach((product) => {
+      if (product.expirationDate) {
+        const expirationDate = new Date(product.expirationDate);
+        const timeDifference = expirationDate.getTime() - currentDate.getTime();
+        const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+        if (daysDifference <= 3 && daysDifference > 0) { //3 dni - można dostosować.
+          expiringProducts.push(product);
+        }
+      }
+    });
+
+    if (expiringProducts.length > 0) {
+      this.sendExpirationNotification(expiringProducts);
     }
+  }
+
+  sendExpirationNotification(products: any[]) {
+    const productNames = products.slice(0, 5).map(product => product.name).join(', ');
+    //Jesli jest wiecej niz 5 to nie wyswietlamy.
+    const moreProductsText = products.length > 5 ? `i ${products.length - 5} innych produktów...` : '';
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready
+        .then((registration) => {
+          const notificationTitle = `Zbliża się termin ważności produktów`;
+          const notificationBody = `Produkty: ${productNames} mają datę ważności za 3 dni! ${moreProductsText}`;
+          console.log(`Produkty: ${productNames} mają datę ważności za 3 dni! ${moreProductsText}`);
+          registration.showNotification(notificationTitle, {
+            body: notificationBody,
+            tag: 'expiration-notification',
+          });
+        })
+        .catch((err) => console.error('Błąd podczas wyzwalania powiadomienia:', err));
+    } else {
+      console.error('Service Worker nie jest dostępny w tej przeglądarce.');
+    }
+  }
+
+  searchProducts(name: string): Observable<any> {
+    return this.http.get<any[]>(`http://localhost:5000/api/product?name=${name}`);
+  }
+
+  formatDate(date: Date): string {
+    return this.datePipe.transform(date, 'shortDate') ?? '';
   }
 }
