fb933c441f8aae32c3a5d05acd32f0c22e330c61
Klaudia Klim <klaudia.n.klim@gmail.com>
2024-12-12 00:36:53 +0100

    Fixed stuff again - Fixed home page containers - Fixed categories in product list

--- Spizarnia/src/app/components/partials/product-model-edit-dialog/product-model-edit-dialog.component.ts
+++ Spizarnia/src/app/components/partials/product-model-edit-dialog/product-model-edit-dialog.component.ts
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';  // Ensure FormsModule is imported
   selector: 'app-product-model-edit-dialog',
   standalone: true,
   templateUrl: './product-model-edit-dialog.component.html',
-  styleUrls: ['./product-model-edit-dialog.component.css'],
+ // styleUrls: ['./product-model-edit-dialog.component.css'],
   imports: [CommonModule, MatFormFieldModule, FormsModule],  // Ensure these are modules, not components
 })
 export class ProductModelEditDialogComponent {
@@ -20,26 +20,22 @@ export class ProductModelEditDialogComponent {
     public dialogRef: MatDialogRef<ProductModelEditDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    // Initialize fields with the data passed from the parent component
     this.updatedPrice = data.price;
     this.updatedCategory = data.category;
     this.updatedType = data.type;
   }
 
   onSave(): void {
-    // Convert price to a number before sending it back to the parent
     const updateProduct = {
-      price: parseFloat(this.updatedPrice),  // Ensure price is a number
+      price: parseFloat(this.updatedPrice),  
       category: this.updatedCategory,
       type: this.updatedType
     };
 
-    // Pass updated data back to the parent
     this.dialogRef.close(updateProduct);
   }
 
   onCancel(): void {
-    // Close the dialog without sending any data
     this.dialogRef.close();
   }
 }
