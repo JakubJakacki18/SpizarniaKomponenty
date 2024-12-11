48312848bdedfd3879b802e0160cb6baf911190d
Jakub <jakub.jakacki18@gmail.com>
2024-12-11 17:31:47 +0100

    - recipe serwis

---
+++ Spizarnia/src/app/services/recipe.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Recipe } from '../../../../Spizarnia-backend/src/models/Recipe'
+import { RECIPES } from '../shared/constances/urls';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  constructor(private http: HttpClient) { }
+
+  getAllRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(RECIPES);
+  }
+
+  getRecipeById(id: number): Observable<Recipe> {
+    return this.http.get<Recipe>(RECIPES+id);
+  }
+
+  addRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.http.post<Recipe>(RECIPES, recipe);
+  }
+
+  updateRecipe(id: number, recipe: Recipe): Observable<Recipe> {
+    return this.http.put<Recipe>(RECIPES+id, recipe);
+  }
+
+  deleteRecipe(id: number): Observable<void> {
+    return this.http.delete<void>(RECIPES+id);
+  }
+}
--- Spizarnia/src/app/shared/constances/urls.ts
+++ Spizarnia/src/app/shared/constances/urls.ts
@@ -6,4 +6,5 @@ export const PRODUCT_MODELS = BASE_URL + "/api/productModel/"
 export const PRODUCTS = BASE_URL + "/api/product/"
 export const CATEGORIES = BASE_URL + "/api/category/"
 export const CONTAINERS = BASE_URL + "/api/container/"
+export const RECIPES = BASE_URL + "/api/recipe/"
 
