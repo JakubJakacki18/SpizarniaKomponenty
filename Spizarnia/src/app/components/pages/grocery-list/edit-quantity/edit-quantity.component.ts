5f3c92bf6b0dfdc7bbaf9b01b499366e6204fb57
Jakub Jakacki <jakub.jakacki18@gmail.com>
2025-01-08 15:42:01 +0100

    - usunięcie błędów, które były powodowane przez brak importów, - przy odkliknięciu okna wartość podczas zamykania okna edycji jest równa -1

--- Spizarnia/src/app/components/pages/grocery-list/edit-quantity/edit-quantity.component.ts
+++ Spizarnia/src/app/components/pages/grocery-list/edit-quantity/edit-quantity.component.ts
@@ -1,12 +1,13 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { ListOfProductsToBuy } from '../../../../../../../Spizarnia-backend/src/models/ListOfProductsToBuy';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-edit-quantity',
-  imports: [MatFormFieldModule, FormsModule],
+  imports: [MatFormFieldModule, FormsModule,MatDialogModule,MatInputModule],
   templateUrl: './edit-quantity.component.html',
   styleUrl: './edit-quantity.component.css'
 })
@@ -16,7 +17,6 @@ export class EditQuantityComponent {
     @Inject(MAT_DIALOG_DATA) public productOnList: ListOfProductsToBuy
   ) 
   {
-    console.log(productOnList); 
     this.newQuantity = productOnList.quantity;
   }
   newQuantity: number;
@@ -26,5 +26,10 @@ export class EditQuantityComponent {
 
   onCancel(): void {
     this.dialogRef.close(-1);
+  }
+  ngOnDestroy(): void {
+    if (this.dialogRef) {
+      this.dialogRef.close(-1);
+    }
   }
 }
