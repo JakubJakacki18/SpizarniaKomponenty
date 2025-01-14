4d8b98114a464740df5b5a1cc56a982b1448bea7
Jakub <jakub.jakacki18@gmail.com>
2025-01-14 21:59:57 +0100

    - Dołączenie biblioteki MatInput

--- Spizarnia/src/app/components/partials/product-model-edit-dialog/product-model-edit-dialog.component.ts
+++ Spizarnia/src/app/components/partials/product-model-edit-dialog/product-model-edit-dialog.component.ts
@@ -4,13 +4,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';  // Ensure FormsModule is imported
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-product-model-edit-dialog',
   standalone: true,
   templateUrl: './product-model-edit-dialog.component.html',
   styleUrls: ['./product-model-edit-dialog.component.css'],
-  imports: [CommonModule, MatFormFieldModule, FormsModule, MatButtonModule],  // Ensure these are modules, not components
+  imports: [CommonModule, MatFormFieldModule, FormsModule, MatButtonModule,MatInputModule],  // Ensure these are modules, not components
 })
 export class ProductModelEditDialogComponent {
   updatedPrice: string;  // Price as string for binding with input
