1ae46a1a6726eb37cab2894987fc17bb0322a38d
JakubKazimiruk46 <154755194+JakubKazimiruk46@users.noreply.github.com>
2025-01-15 08:43:37 +0100

    Hotfix do kaskadowego

--- Spizarnia-backend/src/models/Ingredient.ts
+++ Spizarnia-backend/src/models/Ingredient.ts
@@ -1,14 +1,18 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable, BeforeRemove, getManager } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable, JoinColumn, BeforeRemove, getManager } from "typeorm";
 import { DataSource } from 'typeorm';
 import { ProductModel } from "./ProductModel";
 import {Recipe} from "./Recipe"
+
 
 @Entity()
 export class Ingredient {
     @PrimaryGeneratedColumn()
     id!: number;
 
-    @ManyToOne(() => ProductModel, (productModel) => productModel.ingredients)
+    @ManyToOne(() => ProductModel, (productModel) => productModel.ingredients, {
+      onDelete: "CASCADE"
+      })
+      @JoinColumn({name: "productModelId"})
     productModel?: ProductModel;
   
     @Column("decimal")
