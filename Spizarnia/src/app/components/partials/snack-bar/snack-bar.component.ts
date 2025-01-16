5a78d4201ab33ad614adeddc41664f4b56e177a6
Jakub Jakacki <jakub.jakacki18@gmail.com>
2025-01-16 11:45:29 +0100

    dodanie komponentu SnackBar, teraz wyświetla się poprawnie podświetlenie przycisku

---
+++ Spizarnia/src/app/components/partials/snack-bar/snack-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject, inject } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MAT_SNACK_BAR_DATA, MatSnackBarAction, MatSnackBarActions, MatSnackBarLabel, MatSnackBarRef } from '@angular/material/snack-bar';
+@Component({
+  selector: 'app-snack-bar',
+  imports: [MatButtonModule, MatSnackBarLabel, MatSnackBarActions, MatSnackBarAction],
+  templateUrl: './snack-bar.component.html',
+  styleUrl: './snack-bar.component.css'
+})
+export class SnackBarComponent {
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) {}
+  snackBarRef = inject(MatSnackBarRef);
+}
