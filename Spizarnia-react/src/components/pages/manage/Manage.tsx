8f7e8f77581d511c36de0424d34264f1362e6a72
Jakub <jakub.jakacki18@gmail.com>
2025-01-21 20:48:34 +0100

    - wyświetlanie przycisków w manage

--- Spizarnia-react/src/components/pages/manage/Manage.tsx
+++ Spizarnia-react/src/components/pages/manage/Manage.tsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+
+function classNameSelectorNavButton(isActive : boolean)
+{
+    return isActive ? "active-link" : "action-button"
+}
 
 function Manage() {
     return (
-        <div>
-            <h1>Manage</h1>
-            <p>To jest strona główna aplikacji.</p>
-            <Outlet />
+        <>        
+        <div className="manage-header-container">
+            <div className="title">Zarządzaj</div>
+                <div className="button-group">
+                    <NavLink to="/manage/productModelView" className={({ isActive }) => classNameSelectorNavButton(isActive)}>Zarządzaj produktami w katalogu</NavLink >
+                    <NavLink to="/manage/productModel" className={({ isActive }) => classNameSelectorNavButton(isActive)}>Dodaj produkt do katalogu</NavLink >
+                    <NavLink to="/manage/product" className={({ isActive }) => classNameSelectorNavButton(isActive)}>Dodaj produkt do spiżarni</NavLink >
+                    <NavLink to="/manage/recipe" className={({ isActive }) => classNameSelectorNavButton(isActive)}>Dodaj przepis</NavLink >
+                    <NavLink to="/manage/category" className={({ isActive }) => classNameSelectorNavButton(isActive)}>Dodaj kategorię</NavLink >
+                </div>
         </div>
+
+        <div className="site-content">
+            <Outlet />
+        </div>    
+        </>
     );
 }
 
