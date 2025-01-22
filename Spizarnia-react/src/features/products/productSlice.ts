f13050f313de37bf7fcfaa8517c8230414fbade6
Jakub <jakub.jakacki18@gmail.com>
2025-01-22 02:23:15 +0100

    - dodanie usuwania produktu do productSlice

--- Spizarnia-react/src/features/products/productSlice.ts
+++ Spizarnia-react/src/features/products/productSlice.ts
@@ -1,8 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = {
-    products: [],
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { Status } from "../../shared/constances/statusType.ts";
+import AxiosApi from "../../api/axiosApi.ts";
+
+interface ProductState {
+    products: any[]; 
+    status: Status;
+    error: any; 
 }
 
+const initialState : ProductState = {
+    products: [],
+    status: Status.idle,
+    error: null,
+}
+
+export const deleteProduct = createAsyncThunk(
+    'products/deleteProduct',
+    async (productId, { rejectWithValue }) => {
+      try {
+        await AxiosApi.axiosProducts.delete(`/${productId}`);
+        return productId; 
+      } catch (error) {
+        return rejectWithValue(error.response?.data || 'Error occurred');
+      }
+    }
+  );
 
 const productSlice = createSlice({
     name: "products",
@@ -12,6 +34,22 @@ const productSlice = createSlice({
             state.products = payload;
         },
     },
+    extraReducers: (builder) => {
+        builder
+        // Obsługa akcji usuwania
+        .addCase(deleteProduct.pending, (state) => {
+          state.status = Status.loading;
+        })
+        .addCase(deleteProduct.fulfilled, (state, action) => {
+          state.status = Status.success;
+          // Usuwamy produkt z listy po ID
+          state.products = state.products.filter((item) => item.id !== action.payload);
+        })
+        .addCase(deleteProduct.rejected, (state, action) => {
+          state.status = Status.error;
+          state.error = action.payload;
+        });
+    }
     
 });
 
