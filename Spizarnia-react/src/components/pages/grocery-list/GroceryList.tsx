0f0cf9012aae9aba2f134e57e1e0cfaf00e2d8c1
Klaudia Klim <klaudia.n.klim@gmail.com>
2025-01-31 23:36:12 +0100

    search bar in grocery list

--- Spizarnia-react/src/components/pages/grocery-list/GroceryList.tsx
+++ Spizarnia-react/src/components/pages/grocery-list/GroceryList.tsx
@@ -1,70 +1,66 @@
-import { AxiosResponse } from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import AxiosApi from "../../../api/axiosApi.ts";
+import { AxiosResponse } from "axios";
 import { addListOfProductsToBuy, deleteGroceryList } from "../../../features/listOfProductsToBuy/listOfProductsToBuySlice.ts";
-import { NavLink } from "react-router-dom";
-import StyleFunctions from "./../../../shared/styleFunctions.ts"
-import GroceryTable from "./GroceryTable.tsx";
+import AxiosApi from "../../../api/axiosApi.ts";
 import { AppDispatch } from "../../../features/store.ts";
+import GroceryTable from "./GroceryTable.tsx";
 import ConfirmationDialog from "../shared/ConfirmationDialog.tsx";
 
-
-
 function GroceryList() {
-    const dispatch : AppDispatch= useDispatch();
+    const dispatch: AppDispatch = useDispatch();
+    const [searchTerm, setSearchTerm] = useState<string>(""); // Stan do wyszukiwania
     const [openConfirmationDialog, setConfirmationDialog] = useState(false);
+
     const handleDeleteList = () => {
-      dispatch(deleteGroceryList());
+        dispatch(deleteGroceryList());
     };
+
     useEffect(() => {
         const fetchListOfProductsToBuy = async () => {
-        try {
-            const response : AxiosResponse = await AxiosApi.axiosListOfProductsToBuy.get('')//.catch((error) => {console.error('Error: ', error)});
-            dispatch(addListOfProductsToBuy(response.data));
-            console.log(response.data);
-        }
-        catch(error) {
-            console.error('Error: ', error);
-        }
-        }
+            try {
+                const response: AxiosResponse = await AxiosApi.axiosListOfProductsToBuy.get('');
+                dispatch(addListOfProductsToBuy(response.data));
+            } catch (error) {
+                console.error('Error: ', error);
+            }
+        };
         fetchListOfProductsToBuy();
-        
     }, [dispatch]);
+
     return (
         <>
-        <div className="header-container">
-        <div className="title">Lista zakupów</div>
-        <div className="search-bar">
-          <input type="text"
-                 placeholder="Wyszukaj przepis..."/>
-          <button className="action-button" >
-            Szukaj
-          </button>
-          <button className="action-button" onClick={()=>
-          {
-            setConfirmationDialog(true)
-          }
-          }>
-          Usuń listę
-          </button>
-        </div>
-      </div>
-      <div className="site-content">
-        <GroceryTable/>
-      </div>
-      <ConfirmationDialog
-      title={"Usuwanie listy zakupów"}
-      content={`Czy chcesz usunąć całą listę produktów?`}
-      openConfirmationDialog={openConfirmationDialog}
-      setConfirmationDialog={setConfirmationDialog}
-      actionFunction={handleDeleteList}
-      />
+            <div className="header-container">
+                <div className="title">Lista zakupów</div>
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        placeholder="Wyszukaj produkt..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <button className="action-button" onClick={() => setConfirmationDialog(true)}>
+                        Usuń listę
+                    </button>
+                </div>
+            </div>
+            <div className="site-content">
+                <GroceryTable searchTerm={searchTerm} />
+            </div>
+
+            <ConfirmationDialog
+                title={"Usuwanie listy zakupów"}
+                content={`Czy chcesz usunąć całą listę produktów?`}
+                openConfirmationDialog={openConfirmationDialog}
+                setConfirmationDialog={setConfirmationDialog}
+                actionFunction={handleDeleteList}
+            />
         </>
     );
 }
 
-export default GroceryList
+export default GroceryList;
+
 
 
 //  <ConfirmationDialog
