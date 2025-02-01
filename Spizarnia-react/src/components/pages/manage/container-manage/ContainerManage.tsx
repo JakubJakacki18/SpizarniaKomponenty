80e1c928bfd9ff2dbf96d94c32e3c000ad01aa88
Klaudia Klim <klaudia.n.klim@gmail.com>
2025-02-02 00:37:57 +0100

    tabela kategorie style

--- Spizarnia-react/src/components/pages/manage/container-manage/ContainerManage.tsx
+++ Spizarnia-react/src/components/pages/manage/container-manage/ContainerManage.tsx
@@ -1,79 +1,126 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from 'react-router-dom';
-import { DataGrid } from "@mui/x-data-grid/DataGrid";
-import { addProducts, getAllProducts } from "../../../../features/products/productSlice.ts";
+import { useParams } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import { deleteProduct, addProducts, getAllProducts } from "../../../../features/products/productSlice.ts";
 import { AppDispatch } from "../../../../features/store.ts";
 import { AxiosResponse } from "axios";
 import AxiosApi from "../../../../api/axiosApi.ts";
+import ConfirmationDialog from "../../shared/ConfirmationDialog.tsx";
+import dayjs from "dayjs";
 
-function ContainerManage() {
+const ContainerManage = () => {
     const { categoryName } = useParams<{ categoryName: string }>();
-    const dispatch : AppDispatch = useDispatch();
+    const dispatch: AppDispatch = useDispatch();
+    const [openConfirmationDialog, setConfirmationDialog] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState<string>("");
+    const [selectedProductId, setSelectedProductId] = useState<string>("");
+
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response: AxiosResponse = await AxiosApi.axiosProducts.get('');
+                const response: AxiosResponse = await AxiosApi.axiosProducts.get("");
                 dispatch(addProducts(response.data));
-                console.log(response.data);
             } catch (error) {
-                console.error('Error: ', error);
+                console.error("Błąd pobierania produktów:", error);
             }
         };
         fetchProducts();
     }, [dispatch]);
 
+    const products = useSelector(getAllProducts).filter((product) => product.categoryName === categoryName);
 
-    const products = useSelector(getAllProducts).filter(product => product.categoryName===categoryName)
+    const handleDelete = (productId: string) => {
+        dispatch(deleteProduct(productId));
+        setConfirmationDialog(false);
+    };
+
     const columns = [
-                { field: "id", headerName: "ID", width: 50, headerClassName: 'table-header' },
-                { field: "name", headerName: "Nazwa", width: 150, headerClassName: 'table-header' },
-                { field: "quantity", headerName: "Ilość", width: 100, headerClassName: 'table-header' },
-                { field: "categoryName", headerName: "Kategoria", width: 150, headerClassName: 'table-header' },
-                { field: "purchaseDate", headerName: "Data Zakupu", width: 150, headerClassName: 'table-header' },
-                { field: "expirationDate", headerName: "Data Ważności", width: 180, headerClassName: 'table-header' }]
+        { field: "id", headerName: "ID", width: 50, headerClassName: "table-header" },
+        { field: "name", headerName: "Nazwa", width: 150, headerClassName: "table-header" },
+        { field: "quantity", headerName: "Ilość", width: 100, headerClassName: "table-header" },
+        { field: "categoryName", headerName: "Kategoria", width: 150, headerClassName: "table-header" },
+        { field: "purchaseDate", headerName: "Data Zakupu", width: 180, headerClassName: "table-header" },
+        { field: "expirationDate", headerName: "Data Ważności", width: 180, headerClassName: "table-header" },
+        {
+            field: "akcje",
+            headerName: "Akcje",
+            width: 180,
+            headerClassName: "table-header",
+            renderCell: (params) => (
+                <button
+                    onClick={() => {
+                        setSelectedProduct(params.row.name);
+                        setSelectedProductId(params.row.id.toString());
+                        setConfirmationDialog(true);
+                    }}
+                    className="action-edit-button">
+                    Usuń
+                </button>
+            ),
+        },
+    ];
 
-    const table = products?.length > 0 ? (
-                <div className="table-container">
-                    <DataGrid
-                        rows={products}
-                        columns={columns}
-                        disableRowSelectionOnClick
-                        sx={{
-                            border: "1px solid var(--background-color)",
-                            fontFamily: '"Poppins", "Arial Black"',
-                            '& .MuiDataGrid-cell': {
-                                border: '1px solid var(--font-color)',
-                                backgroundColor: 'var(--font-color)',
-                                fontFamily: '"Poppins", "Arial Black"',
-                            },
-                            '& .MuiDataGrid-columnHeader': {
-                                border: '1px solid var(--font-color)',
-                                backgroundColor: 'var(--font-color)',
-                                fontFamily: '"Poppins", "Arial Black"',
-                            },
-                            '& .MuiDataGrid-row': {
-                                borderBottom: '1px solid var(--font-color)',
-                                backgroundColor: 'var(--font-color)',
-                                fontFamily: '"Poppins", "Arial Black"',
-                            },
-                        }}
-                    />
-                </div>
-            ) : (
-                <div className="no-data">Brak produktów w kontenerze {categoryName}.</div>
-            );
-    const contentToRender = (!products) ?  
-    <div className="no-data"> Dane nie zostały prawidłowo doczytane do kategorii {categoryName}.</div> : table
-    console.log("Szukana kategoria: ",products)
-    return (
-        <div className="site-content">
-            <div className="title-manage">Produkty z kategorii {categoryName}</div>
-                <div className="container-wrapper">
-                    {contentToRender}
-                </div>
+    const today = dayjs();
+    const table = products.length > 0 ? (
+        <div className="table-container">
+            <DataGrid
+                rows={products}
+                columns={columns}
+                disableRowSelectionOnClick
+                autoHeight
+                getRowClassName={(params) => {
+                    const expirationDate = dayjs(params.row.expirationDate);
+                    const daysToExpiration = expirationDate.diff(today, "day");
+
+                    if (daysToExpiration < 0) return "expired";
+                    if (daysToExpiration < 1) return "almost-expired";
+                    if (daysToExpiration <= 3) return "soon-expiring";
+                    return "expiring";
+                }}
+                sx={{
+                    border: "1px solid var(--background-color)",
+                    fontFamily: '"Poppins", "Arial Black"',
+                    "& .MuiDataGrid-cell": {
+                        border: "1px solid var(--font-color)",
+                        backgroundColor: "var(--font-color)",
+                        fontFamily: '"Poppins", "Arial Black"',
+                    },
+                    "& .MuiDataGrid-columnHeader": {
+                        border: "1px solid var(--font-color)",
+                        backgroundColor: "var(--font-color)",
+                        fontFamily: '"Poppins", "Arial Black"',
+                    },
+                    "& .MuiDataGrid-row": {
+                        borderBottom: "1px solid var(--font-color)",
+                        backgroundColor: "var(--font-color)",
+                        fontFamily: '"Poppins", "Arial Black"',
+                    },
+                }}
+            />
         </div>
+    ) : (
+        <div className="no-data">Brak produktów w kategorii {categoryName}.</div>
     );
-}
+
+    return (
+        <>
+            <div className="manage-content">
+                <div className="title-manage">Produkty z kategorii {categoryName}</div>
+            <div>{table}</div>
+
+            <ConfirmationDialog
+                title="Usuwanie produktu"
+                content={`Czy chcesz usunąć produkt ${selectedProduct}?`}
+                openConfirmationDialog={openConfirmationDialog}
+                setConfirmationDialog={setConfirmationDialog}
+                actionFunction={handleDelete}
+                dataToFunction={selectedProductId}
+                />
+            </div>
+        </>
+
+    );
+};
 
 export default ContainerManage;
