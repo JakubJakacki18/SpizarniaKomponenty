0107dd583933c70e63f84c5f27d06b833d4b684d
Klaudia Klim <klaudia.n.klim@gmail.com>
2025-02-01 19:58:30 +0100

    search in recipes

--- Spizarnia-react/src/components/pages/recipes/Recipes.tsx
+++ Spizarnia-react/src/components/pages/recipes/Recipes.tsx
@@ -1,8 +1,8 @@
-import { AxiosResponse } from "axios";
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+﻿import { AxiosResponse } from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import AxiosApi from "../../../api/axiosApi.ts";
-import { addRecipes, fetchRecipesWithExecutionStatus } from "../../../features/recipes/recipeSlice.ts";
+import { fetchRecipesWithExecutionStatus, getAllRecipes } from "../../../features/recipes/recipeSlice.ts";
 import { NavLink } from "react-router-dom";
 import StyleFunctions from "../../../shared/styleFunctions.ts";
 import RecipesList from "./RecipesList.tsx";
@@ -10,44 +10,43 @@ import { AppDispatch } from "../../../features/store.ts";
 
 function Recipes() {
     const dispatch = useDispatch<AppDispatch>();
+    const allRecipes = useSelector(getAllRecipes); // Pobranie przepisów z Redux
+    const [searchTerm, setSearchTerm] = useState(""); // Stan wyszukiwarki
+
     useEffect(() => {
-        const fetchRecipes = async () => {
-        try {
-            //const response : AxiosResponse = await AxiosApi.axiosRecipes.get('')//.catch((error) => {console.error('Error: ', error)});
-            dispatch(fetchRecipesWithExecutionStatus())
-            //dispatch(addRecipes(response.data));
-            //console.log(response.data);
-        }
-        catch(error) {
-            console.error('Error: ', error);
-        }
-        }
-        fetchRecipes();
-        
+        dispatch(fetchRecipesWithExecutionStatus());
     }, [dispatch]);
-    function onSearch(): void {
-        throw new Error("Function not implemented.");
-    }
+
+    // Filtrujemy przepisy na podstawie wyszukiwanego hasła
+    const filteredRecipes = allRecipes.filter((recipe) =>
+        recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
         <>
-        <div className="header-container">
-        <div className="title">Przepisy</div>
-        <div className="search-bar">
-          <input type="text"
-                 placeholder="Wyszukaj przepis..."/>
-          <button className="action-button" onClick={onSearch}>
-            Szukaj
-          </button>
-        <NavLink to="/manage/recipe" className={({ isActive }) => StyleFunctions.classNameSelectorNavButton(isActive)}>Dodaj przepis</NavLink >
-        </div>
-      </div>
-      <div className="site-content">
-        <RecipesList/>
-      </div>
+            <div className="header-container">
+                <div className="title">Przepisy</div>
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        placeholder="Wyszukaj przepis..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <NavLink
+                        to="/manage/recipe"
+                        className={({ isActive }) => StyleFunctions.classNameSelectorNavButton(isActive)}
+                    >
+                        Dodaj przepis
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="site-content">
+                <RecipesList recipes={filteredRecipes} />
+            </div>
         </>
     );
 }
 
 export default Recipes;
-
